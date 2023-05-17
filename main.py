ffa22dbfca7c8fc80e2f1543a12048bcb16b7e4e
matardy <gutemberg.mendoza@epn.edu.ec>
2023-05-17 09:54:13 -0500

    Basic structure calculator

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+
+def sum_function(a:float, b:float)->float:
+    pass
+
+
+def subtract_function(a: float, b: float) -> float:
+    pass
+
+
+def multiply_function(a: float, b: float) -> float:
+    pass
+
+
+
+
+def menu():
+    prompt = f"""
+    Very simple calculator.
+    1. Sum two numbers
+    2. subtract two numbers
+    3. Multiply two numbers
+    Choose one:
+    """
+    print(prompt)
+
+
+def main():
+    while True:
+        menu()
+        selection = int(input())
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        if (selection == 1):
+            a = input("First number:")
+            b = input("Second number:")
+            print(sum_function(float(a),float(b)))
+        elif(selection==2):
+            a = input("First number:")
+            b = input("Second number:")
+            print(subtract_function(a, b))
+        elif (selection == 3):
+            a = input("First number:")
+            b = input("Second number:")
+            print(multiply_function(float(a), float(b)))
+
+
+if __name__ == "__main__":
+    main()
