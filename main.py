2cd795d71b68dd64d8d10f56a3934080ca5f6fe9
isaac reyes <isaac.reyes@epn.edu.ec>
2023-05-17 10:03:11 -0500

    Substract function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def sum_function(a: float, b: float) -> float:
 
 
 def subtract_function(a: float, b: float) -> float:
-    pass
+    return a-b
 
 
 def multiply_function(a: float, b: float) -> float:
@@ -37,7 +37,7 @@ def main():
         elif (selection == 2):
             a = input("First number:")
             b = input("Second number:")
-            print(subtract_function(a, b))
+            print(subtract_function(float(a),float(b)))
         elif (selection == 3):
             a = input("First number:")
             b = input("Second number:")
